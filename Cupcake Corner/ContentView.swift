bb26ac0fdfdd44432113a55ba32e19b113144551
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-06-10 22:48:51 +0100

    Haptic Engine Practise

--- Cupcake Corner/ContentView.swift
+++ Cupcake Corner/ContentView.swift
@@ -5,50 +5,70 @@
 //  Created by Mehmet Alp Sönmez on 10/06/2024.
 //
 
+import CoreHaptics
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
+@Observable
+class User: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _name = "name"
+    }
+    var name = "Taylor"
+    
 }
 
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
 
 struct ContentView: View {
-    @State private var results = [Result]()
-    
+    @State private var counter = 0
+    @State private var engine: CHHapticEngine?
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .task {
-            await loadData()
+        Button("Play Haptic:", action: complexSuccess)
+            .onAppear(perform: prepareHaptics)
+    }
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an Error creating the Engine: \(error.localizedDescription)")
         }
     }
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=eminemt&entity=song") else {
-            print("Invalid URL")
-            return
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        for i in stride(from: 0, to: 10, by: 0.1){
+            
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient
+                                      , parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+        
+        
+        for i in stride(from: 0, to: 10, by: 0.1){
+            
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1-i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1-i))
+            let event = CHHapticEvent(eventType: .hapticTransient
+                                      , parameters: [intensity, sharpness], relativeTime: i + 1)
+            events.append(event)
         }
         
         do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-            
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
         } catch {
-            print("Invalid Data")
+            print("Failed to play pattern: \(error.localizedDescription)")
         }
+        
+        
     }
 }
 
