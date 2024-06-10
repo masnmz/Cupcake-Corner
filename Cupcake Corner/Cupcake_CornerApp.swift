91092ef7bea7adc6501ae14460188ff514ec1a90
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-06-10 15:19:08 +0100

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Mehmet Alp Sönmez on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
