1b41218284f9e1bea8cddeeebd8b74ed08bd9535
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-06-11 14:47:36 +0100

    Address View

---
+++ Cupcake Corner/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  Cupcake Corner
+//
+//  Created by Mehmet Alp Sönmez on 11/06/2024.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
